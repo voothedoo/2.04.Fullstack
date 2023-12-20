bbfa76671b8590f834c917ed031ae77fa4ae269d
Alex M - laptop <vootehdoo@gmail.com>
2023-12-20 16:19:50 +0100

    Change localhost

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 import mariadb from "mariadb";
 
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || localhost;
+const HOST = process.env.HOST || "localhost";
 const app = express();
 app.use(express.json());
 app.use(cors());
